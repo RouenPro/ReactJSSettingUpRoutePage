b02f2cb45457a48db07864eec4a083037cbbd50f
chamrouen <sannchamrouen17@kit.edu.kh>
2020-02-20 00:06:09 +0700

    Adding react-router-dom

--- setup-route-page/src/App.js
+++ setup-route-page/src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
 const App = () => {
-  return <h1>Hello world</h1>
+  return (
+    <h1>Hello world</h1>
+    <Router>
+      <Route path='/'>
+        <Users/>
+      </Route>
+      <Redirect to="/"/>
+    </Router>
+  );
 };
 export default App;
