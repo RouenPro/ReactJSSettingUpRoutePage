cadb25942ddac691c8f4bf7523b882445c0b5e33
chamrouen <sannchamrouen17@kit.edu.kh>
2020-02-20 01:19:46 +0700

    Last update of creating routing of reactjs

--- setup-route-page/src/App.js
+++ setup-route-page/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 
-import Users from "./user/Users";
+import Users from "./user/pages/Users";
+import NewPlace from "./places/pages/NewPlace";
 
 const App = () => 
 {
   return (
     <Router>
-      <Route path='/'>
-        <Users/>
-      </Route>
-      <Redirect to="/Rouen"/>
+      <switch>
+        <Route path='/' exact>
+          <Users/>
+        </Route>
+        <Route path='/places/new' exact>
+          <NewPlace></NewPlace>
+        </Route>
+      </switch>
     </Router>
   );
 };
+
 export default App;
---
+++ setup-route-page/src/places/pages/NewPlace.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NewPlace = () => {
+    return (<h1>Hell from new NewPlace</h1>)
+};
+
+export default NewPlace;
